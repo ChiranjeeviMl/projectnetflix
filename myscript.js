5e45b16476d84b031312d342b6fa132a26815fa5
ChiranjeeviMl <chiranjeevigarikipati99@gmail.com>
2022-05-10 13:09:05 +0530

    chiru

---
+++ myscript.js
@@ -0,0 +1,22 @@
+let day_and_night = 0;
+
+function change_mode() {
+    if (day_and_night % 2 == 0) {
+        document.getElementById('change-mode').classList.remove('bi-brightness-high-fill');
+        document.getElementById('change-mode').classList.add('bi-moon-stars-fill');
+        document.body.style.backgroundColor = "white";
+        day_and_night++;
+    } else {
+        document.getElementById('change-mode').classList.remove('bi-moon-stars-fill');
+        document.getElementById('change-mode').classList.add('bi-brightness-high-fill');
+        document.body.style.backgroundColor = "black";
+        day_and_night--;
+    }
+}
+function loadCoupon(){
+    document.getElementById("coupon").style.display="block"
+}
+
+function closeCoupon(){
+    document.getElementById("coupon").style.display="none"
+}
